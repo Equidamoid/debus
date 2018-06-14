d06f8f0f4d7785bfb43c94a0df0db1bda2174fb9
Vladimir Shapranov <equidamoid@gmail.com>
2018-06-14 20:37:11 +0200

    some metadata in setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,11 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
+    name='pybus',
+    version='0.1',
+    description='DBus wire protocol implementation',
+    author='Vladimir Shapranov',
+    author_email='equidamoid@gmail.com',
+    url='https://github.com/Equidamoid/pybus',
     ext_modules = cythonize("pybus/pybus_struct.pyx")
 )
