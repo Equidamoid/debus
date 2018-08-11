acaf5bc3e3531ca30ae7948e56caa6b6665b3b10
Vladimir Shapranov <equidamoid@gmail.com>
2018-08-11 20:55:08 +0200

    Move low-level connection to a separate file and rename it to WireConnection

--- pybus/connection.py
+++ pybus/connection.py
@@ -1,138 +1,18 @@
 import asyncio
 import logging
-import binascii
-import os
 import pybus.types
-from .pybus_struct import InputBuffer, OutputBuffer
+from pybus.wire import WireConnection
 from .message import Message, make_mesage, MessageType, HeaderField
 logger = logging.getLogger(__name__)
 from lxml import etree
 import io
-import inspect
+
 try:
     import typing
-    StringOrBytes = typing.Union[str, bytes]
+    if typing.TYPE_CHECKING:
+        StringOrBytes = typing.Union[str, bytes]
 except:
     pass
-from pathlib import Path
-import hashlib
-
-
-class BusConnection:
-    def __init__(self, socket=None, uri=None):
-        assert (socket is None) != (uri is None)
-        self.reader = None          # type: asyncio.StreamReader
-        self.writer = None          # type: asyncio.StreamWriter
-        self.parser = InputBuffer()
-        self.server_guid = None     # type: bytes
-        if uri:
-            kind, params = uri.split(':')
-            params = {k:v for k,v in [i.split('=') for i in params.split(',')]}
-            logger.warning("Uri %r parsed, have to connect via %s with parameters %s", uri, kind, params)
-            if kind == 'tcp':
-                socket = (params['host'], int(params['port']))
-            elif kind == 'unix':
-                socket = params['path']
-        self.socket_path = socket
-
-    async def _check_auth_result(self):
-        result = (await self.reader.readline())
-        result_parts = result.split()
-        status = result_parts[0]
-        logger.warning("Auth result: %r", result)
-        if status == b'OK':
-            self.server_guid = binascii.unhexlify(result_parts[1])
-            logger.warning("Auth ok, server guid: %s", self.server_guid)
-            return True
-        return False
-
-    async def _do_auth_external(self):
-        logger.warning("Attempting external auth")
-        self.writer.write(b"AUTH EXTERNAL %s\r\n" % binascii.hexlify(os.environ['USER'].encode()))
-        return await self._check_auth_result()
-
-    async def _do_auth_anonymous(self):
-        logger.warning("Attempting anonymous auth")
-        self.writer.write(b"AUTH ANONYMOUS\r\n")
-        return await self._check_auth_result()
-
-    async def _do_auth_cookie(self):
-        self.writer.write(b"AUTH DBUS_COOKIE_SHA1 %s\r\n" % binascii.hexlify(os.environ['USER'].encode()))
-        resp = await self.reader.readline()
-        resp_parts = resp.split()
-        if resp_parts[0] == b'REJECTED':
-            logger.warning("Dbus cookie auth rejected")
-            return False
-        elif resp_parts[0] == b'DATA':
-            s_cookie_context, s_cookie_id, s_challenge = binascii.unhexlify(resp_parts[1]).split()
-            cookie_file = Path(os.path.expanduser('~/.dbus-keyrings')) / s_cookie_context.decode()
-            logger.warning("Cookie auth data: %r", binascii.unhexlify(resp_parts[1]))
-            selected_cookie = None
-            for line in cookie_file.open('rb'):
-                cookie_id, cookie_ts, cookie = line.split()
-                if int(s_cookie_id) == int(cookie_id):
-                    logging.warning("Found cookie %s", cookie)
-                    selected_cookie = cookie
-                    break
-
-            assert not selected_cookie is None
-
-            cli_challenge = b'a' * (len(s_challenge) // 2)
-            response = b'%s:%s:%s' % (s_challenge, cli_challenge, cookie)
-            response_hash = hashlib.sha1(response).hexdigest().encode()
-
-            response = binascii.hexlify(b'%s %s' % (cli_challenge, response_hash))
-            self.writer.write(b'DATA %s\r\n' % response)
-            logger.warning("Response to hash: %s, hash: %s, response: %s", response, response_hash, response)
-            return await self._check_auth_result()
-        else:
-            raise RuntimeError("Unexpected response during DBUS_COOKIE_SHA1 auth: %r", resp)
-
-    async def connect_and_auth(self):
-        if isinstance(self.socket_path, str):
-            self.reader, self.writer = await asyncio.open_unix_connection(self.socket_path)
-        else:
-            self.reader, self.writer = await asyncio.open_connection(self.socket_path[0], self.socket_path[1])
-        self.writer.write(b"\0")
-        self.writer.write(b"AUTH\r\n")
-        response = await self.reader.readline()
-        auth_available = response.split()[1:]
-        logger.warning("Auth types available: %s", auth_available)
-        if False:
-            pass
-        elif await self._do_auth_cookie():
-            pass
-        elif await self._do_auth_external():
-            pass
-        elif await self._do_auth_anonymous():
-            pass
-        else:
-            logger.error("Can't authenticate with the server")
-        self.writer.write(b'BEGIN\r\n')
-
-    async def recv(self):
-        # type: () -> typing.List[Message]
-        msgs = []
-        while len(msgs) == 0:
-            data = await self.reader.read(1024)
-            if len(data) == 0:
-                logger.error("Connection closed, buffer content: %s", self.parser)
-                raise RuntimeError("Connection closed")
-            msgs = self.parser.feed_data(data)
-        return msgs
-
-    async def send(self, message):
-        # type: (Message) -> None
-        buf = OutputBuffer()
-        try:
-            buf.put_message(message)
-
-        except:
-            logger.error("Unable to serialize message %s", message)
-            raise
-        data = buf.get()
-        assert len(InputBuffer().feed_data(data)) == 1
-        self.writer.write(data)
 
 
 class DBusError(RuntimeError):
@@ -224,7 +104,7 @@ async def get_freedesktop_interface(conn, name=None):
 
 class ClientConnection:
     def __init__(self, socket=None, uri=None):
-        self.bus = BusConnection(socket=socket, uri=uri)
+        self.bus = WireConnection(socket=socket, uri=uri)
         self.futures = {}       # type: typing.Dict[int, asyncio.Future]
         self.freedesktop_interface = None
 
@@ -272,7 +152,7 @@ class ClientConnection:
         err.headers[HeaderField.DESTINATION] = msg.headers[HeaderField.SENDER]
         err.headers[HeaderField.REPLY_SERIAL] = pybus.types.enforce_type(msg.serial, b'u')
         err.headers[HeaderField.ERROR_NAME] = b'space.equi.pybus.Error.NotImplemented'
-        asyncio.ensure_future(self.bus.send(err))
+        self.send_message(err)
 
     def call(self, bus_name, object_path, interface_name, method, signature=None, args=None, timeout=None):
         # type: (StringOrBytes, StringOrBytes, StringOrBytes, StringOrBytes, StringOrBytes, typing.Any, float) -> typing.Any
@@ -295,7 +175,7 @@ class ClientConnection:
                 if not f.done():
                     f.set_exception(TimeoutError("Method call timed out: %s.%s" % (interface_name, method)))
             asyncio.get_event_loop().call_later(timeout, on_timeout)
-        asyncio.ensure_future(self.bus.send(msg))
+        self.send_message(msg)
         return f
 
     def emit(self, object_path: str, interface_name: str, signal_name: str, signature=None, args=None):
@@ -310,7 +190,7 @@ class ClientConnection:
             msg.payload = args
         # print(msg)
         logger.warning("Going to send %s", msg)
-        asyncio.ensure_future(self.bus.send(msg))
+        self.send_message(msg)
 
     def send_message(self, msg: Message):
         asyncio.ensure_future(self.bus.send(msg))
