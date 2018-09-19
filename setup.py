fb75b302c81f94d29382cc58979b683a58a279c5
Vladimir Shapranov <equidamoid@gmail.com>
2018-09-19 22:38:40 +0200

    Try to sed away the variable annontaions to make it work for  py3.5

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 import setuptools
+import os
+import sys
+import setuptools.command.build_py
 from Cython.Build import cythonize
 
+
+class DeannotateAndBuild(setuptools.command.build_py.build_py):
+
+    def build_packages(self):
+        super().build_packages()
+        if sys.version_info[0] == 3 and sys.version_info [1] < 6:
+            print("Installing for older python, trying to clean up annotations...", file=sys.stderr)
+            os.system(r'''set -x; cd %s; find . -name '*.py' | xargs -n1 sed -i '' 's@^\( *[a-zA-Z._]*\):.*=@\1 = @g' ''' % self.build_lib)
+
+
 setuptools.setup(
+    cmdclass={
+        'build_py': DeannotateAndBuild,
+    },
     name='pybus',
     version='0.1',
     description='DBus wire protocol implementation',
