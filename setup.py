fc8f4a91e66649ed6f1d45b596dbb0b4b5b5061b
Vladimir Shapranov <equidamoid@gmail.com>
2018-09-23 18:49:36 +0200

    Do cython packaging the right way, no more cython dependency for `pip install`ing

--- setup.py
+++ setup.py
@@ -2,15 +2,29 @@ import setuptools
 import os
 import sys
 import setuptools.command.build_py
+import typing
 from pathlib import Path
-from Cython.Build import cythonize
+
+def maybe_cythonize(exts: typing.List[setuptools.Extension]):
+    do_it = False
+    for ext in exts:
+        for i in ext.sources:
+            c = Path(i)
+            pyx = Path(i.replace('.c', '.pyx'))
+            if pyx.exists() and ((not c.exists()) or pyx.stat().st_mtime > c.stat().st_mtime):
+                do_it = True
+
+    if do_it:
+        from Cython.Build import cythonize
+        exts = cythonize(exts)
+    return exts
 
 
 class DeannotateAndBuild(setuptools.command.build_py.build_py):
 
     def build_packages(self):
         super().build_packages()
-        if sys.version_info[0] == 3 and sys.version_info [1] < 6:
+        if sys.version_info[0] == 3 and sys.version_info[1] < 6:
             print("Installing for older python, trying to clean up annotations...", file=sys.stderr)
             if sys.platform.startswith('darwin'):
                 sed_i = 'sed -i""'
@@ -31,7 +45,7 @@ setuptools.setup(
     author='Vladimir Shapranov',
     author_email='equidamoid@gmail.com',
     url='https://github.com/Equidamoid/debus',
-    ext_modules=cythonize("debus/marshalling.pyx"),
+    ext_modules=maybe_cythonize([setuptools.Extension('marshalling', ["debus/marshalling.c"])]),
     packages=setuptools.find_packages(),
     install_requires=[
         'lxml',
