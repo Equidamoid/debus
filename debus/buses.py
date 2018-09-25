62973fde7b238b441afa6d99020ab57996dbf4b5
Vladimir Shapranov <equidamoid@gmail.com>
2018-09-25 07:43:51 +0200

    Correct name for session bus env var

--- debus/buses.py
+++ debus/buses.py
@@ -6,4 +6,4 @@ if sys.platform.startswith('darwin'):
     SESSION = 'unix:path=%s' % (os.environ['DBUS_LAUNCHD_SESSION_BUS_SOCKET'],)
 elif sys.platform.startswith('linux'):
     SYSTEM = 'unix:path=/var/run/dbus/system_bus_socket'
-    SESSION = os.environ.get('DBUS_SESSION_BUS_SOCKET', None)
+    SESSION = os.environ.get('DBUS_SESSION_BUS_ADDRESS', None)
